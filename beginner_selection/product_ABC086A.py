1fde1a229dc9fc20a0bd08d7c09394b29a667f30
Archan Ghosh <gharchan@gmail.com>
2026-07-12 17:53:08 +0530

    Create product_ABC086A.py

---
+++ beginner_selection/product_ABC086A.py
@@ -0,0 +1,23 @@
+a,b = map(int,(input().split(" ")))
+
+
+def check(a, b):
+    if a==1:
+        if b%2==0:
+            return "Even"
+        else:
+            return "Odd"
+    elif b==1:
+        if a%2==0:
+            return "Even"
+        else:
+            return "Odd"
+    else:
+        if a%2==0 and b%2==0:
+            return "Even"
+        elif a%2!=0 and b%2!=0:
+            return "Odd"
+        else:
+            return "Even"
+
+print(check(a, b))
